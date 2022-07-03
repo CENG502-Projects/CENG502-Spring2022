26059f551babf3801fdb6707ec962e0553b80ad0
Semih Kaya <semihkayya@hotmail.com>
2022-07-03 23:26:34 +0300

    Remove unnecessary scripts and files

--- Project_Bostanci_Kaya/environments/gridMaze/lossWang/run2/run_dqn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ENV_ID=$1
-
-python -u -B train_dqn.py \
---env_id=${ENV_ID} \
---log_sub_dir=test \
---args="device='cuda'"
--- Project_Bostanci_Kaya/environments/gridRoom/lossWu/run3/run_visualize_reprs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ENV_ID=$1
-
-python -u -B visualize_reprs.py \
---log_sub_dir=laprepr/${ENV_ID}/test \
